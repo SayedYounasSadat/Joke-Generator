70432a61453667274a85743e12df16ddfeea1d3c
Sayed Younas Sadat <130991896+SayedYounasSadat@users.noreply.github.com>
2024-08-22 20:29:02 +0430

    Add files via upload

---
+++ Joke Generator - Beginner.py
@@ -0,0 +1,10 @@
+print("Hey, weolcome to my Joke Generator Machine!\n Input all your date below so that my machine will generage a great joke for you and will make your day!")
+
+adj = input("Enter an adjective here: ")
+noun = input("Enter a noun here: ")
+verb = input("Enter a verb here: ")
+place = input("Enter a place here: ")
+
+story = "The " + adj + " " + noun + " " + verb + " to the" + " " + place + ". " + "It was a/an" + " " + adj + " " + "day."
+
+print(story)
